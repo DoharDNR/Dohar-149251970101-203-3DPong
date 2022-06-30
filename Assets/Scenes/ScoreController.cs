d6be85885ebb2ab70dcb092daea722c62ad30e6e
DoharDNR <cartjonnson@gmail.com>
2022-07-01 02:17:20 +0700

    Adding UI

--- Assets/Scenes/ScoreController.cs
+++ Assets/Scenes/ScoreController.cs
@@ -13,9 +13,9 @@ public class ScoreController : MonoBehaviour
 
     private void Update()
     {
-        skorPemain1.text = manager.pemain1Score.ToString();
-        skorPemain2.text = manager.pemain2Score.ToString();
-        skorPemain3.text = manager.pemain3Score.ToString();
-        skorPemain4.text = manager.pemain4Score.ToString();
+        skorPemain1.text = "Pemain 1 = " + manager.pemain1Score.ToString();
+        skorPemain2.text = "Pemain 2 = " + manager.pemain2Score.ToString();
+        skorPemain3.text = "Pemain 3 = " + manager.pemain3Score.ToString();
+        skorPemain4.text = "Pemain 4 = " + manager.pemain4Score.ToString();
     }
 }
